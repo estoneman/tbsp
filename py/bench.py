eebeb007b052f93ef3f3737c139136ef5f434b72
estoneman <etst9006@colorado.edu>
2023-03-23 12:26:56 -0600

    update testing suite

--- py/bench.py
+++ py/bench.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     domain_list = window.fetch_lines("../data/domains.in")
-    n = 1000 
+    n = 400
     
-    xs = np.arange(0.1, 1.1, 0.1)
+    xs = np.arange(0.1, 0.5, 0.1)
 
-    ys = np.empty(10)
+    ys = np.empty(*xs.shape)
 
-    for i in range(len(xs)):
+    for i in range(xs.shape[0]):
         window_len = int(xs[i]*n)
         k = int(0.5*window_len)
         top, y = window.top_k(domain_list, n, window_len, k, 0.50)
         ys[i] = y
+        domain_list = window.fetch_lines("../data/domains.in")
 
     plt.figure(1)
     plt.plot(xs, ys)
