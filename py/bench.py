6c470d207e5c133d40d106019507c8c8e930457e
estoneman <etst9006@colorado.edu>
2023-03-27 14:22:46 -0600

    updated benchmark script

--- py/bench.py
+++ py/bench.py
@@ -1,30 +1,25 @@
 import window 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 if __name__ == "__main__":
-    domain_list = window.fetch_lines("../data/domains.in")
-    n = 400
+    n = 1000
     
-    xs = np.arange(0.1, 0.5, 0.1)
+    start = 0.10 
+    end = 1.01 
+    step = 0.1
 
+    xs = np.arange(start, end, step)
     ys = np.empty(*xs.shape)
 
     for i in range(xs.shape[0]):
-        window_len = int(xs[i]*n)
-        k = int(0.5*window_len)
-        top, y = window.top_k(domain_list, n, window_len, k, 0.50)
-        ys[i] = y
         domain_list = window.fetch_lines("../data/domains.in")
+        window_len = int(xs[i]*n)
+        k = math.ceil(0.5*window_len)
+        top_k = window.top_k(domain_list, n, window_len, k, 0.50)
+        with open("topk.csv", 'a') as fp:
+            fp.write(f"=== WINDOW SIZE @ {window_len} ===")
+            for pair in top_k:
+                fp.write(f"{pair[0]},{pair[1]}\n")
 
-    plt.figure(1)
-    plt.plot(xs, ys)
-
-    plt.title(f"Sliding Windows @ {n} domains")
-
-    plt.xlabel("Window Size (% of Input Size)")
-    plt.ylabel("Time (s)")
-
-    plt.xticks(xs)
-
-    plt.show()
