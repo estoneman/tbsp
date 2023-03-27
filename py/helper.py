f5d3015418d5b65edce752857b4025e34ef215d3
estoneman <etst9006@colorado.edu>
2023-03-27 14:23:21 -0600

    dataclass name update

--- py/helper.py
+++ py/helper.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
 
 @dataclass
-class AlgResponse:
+class PairsResponse:
     data: list[tuple]
     time_elapsed: float
+
+@dataclass
+class TopKResponse:
+    topk_domains: list[tuple]
+    mean: float
 
