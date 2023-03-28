f1f3f7be63cbd58d92180dcc75e9cdb1b1e20907
estoneman <etst9006@colorado.edu>
2023-03-28 12:41:22 -0600

    add linter

---
+++ py/linter.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+DICT=./words_alpha.txt
+
+for file in `find . -maxdepth 1 -type f -name '*.py'` ; do
+    echo "[+] Linting $file"
+    pylint --spelling-private-dict $DICT \
+           --max-line-length 80 \
+           --min-similarity-lines 2 \
+           --max-locals 20 \
+           --check-quote-consistency y \
+           --good-names i,j,k,n,w \
+           $file
+done
