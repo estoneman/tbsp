4141291f79c24c1f47899a7ad460261460dd4735
estoneman <etst9006@colorado.edu>
2023-03-28 23:21:07 -0600

    update linting rules

--- py/linter.sh
+++ py/linter.sh
@@ -6,7 +6,7 @@ for file in `find . -maxdepth 1 -type f -name '*.py'` ; do
     echo "[+] Linting $file"
     pylint --spelling-private-dict $DICT \
            --max-line-length 80 \
-           --min-similarity-lines 2 \
+           --min-similarity-lines 1 \
            --max-locals 20 \
            --check-quote-consistency y \
            --good-names i,j,k,n,w \
