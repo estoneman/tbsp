96b4b45627f826054d93bf73536f86afd2aa0131
estoneman <etst9006@colorado.edu>
2023-04-01 18:57:06 -0600

    test initial impl of cythonization

--- py/bench.py
+++ py/bench.py
@@ -10,8 +10,8 @@ import matplotlib.colors as mcolors
 import top_k
 from util import fetch_lines
 
-if __name__ == "__main__":
-    n = 100
+def main():
+    n = 1000
     THRESHOLD = 0.70
     START = 10
     END = 101
@@ -20,36 +20,41 @@ if __name__ == "__main__":
     xs = np.arange(START, END, STEP)
     ys = np.empty(*xs.shape)
 
-    for i in range(xs.shape[0]):
-        domain_list = fetch_lines("../data/domains.in")
-        window_len = int(n * (xs[i]/100))
+    # for i in range(xs.shape[0]):
+    domain_list = fetch_lines("../data/domains.in")
+    # window_len = int(n * (xs[i]/100))
+    window_len = 500 
+    top_k.top_k(domain_list, n, window_len, THRESHOLD)
 
-        t1 = time.clock_gettime_ns(time.CLOCK_REALTIME)
+    # t1 = time.clock_gettime_ns(time.CLOCK_REALTIME)
 
-        print(f"scale: {int(xs[i] / STEP)*10}%")
-        top = top_k.top_k(domain_list, n, window_len, THRESHOLD)
+    # print(f"scale: {int(xs[i] / STEP)*10}%")
+    # top = top_k.top_k(domain_list, n, window_len, THRESHOLD)
 
-        t2 = time.clock_gettime_ns(time.CLOCK_REALTIME)
+    # t2 = time.clock_gettime_ns(time.CLOCK_REALTIME)
 
-        ys[i] = round((t2 - t1) / 10e9, 3)
-        # with open(f"../data/topk/top-{window_len}.txt", 'a') as fp:
-        #     for pair in top_k:
-        #         fp.write(f"{pair[0]},{pair[1]}\n")
+    # ys[i] = round((t2 - t1) / 10e9, 3)
+    # with open(f"../data/topk/top-{window_len}.txt", 'a') as fp:
+    #     for pair in top_k:
+    #         fp.write(f"{pair[0]},{pair[1]}\n")
 
-    plt.figure(1)
+    # plt.figure(1)
 
-    plt.title(f"Sliding Window @ {n=} Domains")
-    plt.xlabel("window size (% domain size)")
-    plt.ylabel("time (s)")
+    # plt.title(f"Sliding Window @ {n=} Domains")
+    # plt.xlabel("window size (% domain size)")
+    # plt.ylabel("time (s)")
 
-    plt.xticks(xs)
+    # plt.xticks(xs)
 
-    plt.plot(xs, ys, color=mcolors.BASE_COLORS["k"])
-    marker, stemlines, baseline = plt.stem(xs, ys)
-    plt.setp(marker, "color", mcolors.BASE_COLORS["k"])
-    plt.setp(stemlines, "color", mcolors.BASE_COLORS["k"])
-    plt.setp(stemlines, "linestyle", "dotted")
-    plt.setp(baseline, "color", "grey")
+    # plt.plot(xs, ys, color=mcolors.BASE_COLORS["k"])
+    # marker, stemlines, baseline = plt.stem(xs, ys)
+    # plt.setp(marker, "color", mcolors.BASE_COLORS["k"])
+    # plt.setp(stemlines, "color", mcolors.BASE_COLORS["k"])
+    # plt.setp(stemlines, "linestyle", "dotted")
+    # plt.setp(baseline, "color", "grey")
 
-    print("Plot is shown")
-    plt.show()
+    # print("Plot is shown")
+    # plt.show()
+
+if __name__ == "__main__":
+    main()
