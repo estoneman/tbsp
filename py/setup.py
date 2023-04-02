4f2d06ad9d14378fa582fae68e15c8273a155795
estoneman <etst9006@colorado.edu>
2023-04-01 19:05:49 -0600

    distutils.core -> setuptools

--- py/setup.py
+++ py/setup.py
@@ -1,6 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 
 ext_options = {"compiler_directives": {"profile": True, "language_level": 3},
                "annotate": True}
-setup(ext_modules=cythonize("edit_distance.pyx", **ext_options))
+setup(
+    ext_modules=cythonize("edit_distance.pyx", **ext_options)
+)
