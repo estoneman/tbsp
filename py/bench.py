d379efb7e572ddea486fb02ef9cec90c744d9300
estoneman <etst9006@colorado.edu>
2023-04-03 10:52:35 -0600

    further testing

--- py/bench.py
+++ py/bench.py
@@ -11,8 +11,8 @@ import top_k
 from util import fetch_lines
 
 def main():
-    n = 1000
-    THRESHOLD = 0.70
+    n = 1744
+    THRESHOLD = 0.90
     START = 10
     END = 101
     STEP = 10
@@ -20,20 +20,24 @@ def main():
     xs = np.arange(START, END, STEP)
     ys = np.empty(*xs.shape)
 
-    # for i in range(xs.shape[0]):
-    domain_list = fetch_lines("../data/domains.in")
-    # window_len = int(n * (xs[i]/100))
-    window_len = 500 
-    top_k.top_k(domain_list, n, window_len, THRESHOLD)
+    for i in range(xs.shape[0]):
+        domain_list = fetch_lines("../data/domains.in")
+        # domain_list = fetch_lines(
+        #         "/usr/local/opt/wordlists/rockyou/rockyou.txt"
+        # )
+        window_len = int(n * (xs[i]/100))
+        k = 5
 
-    # t1 = time.clock_gettime_ns(time.CLOCK_REALTIME)
+        t1 = time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)
 
-    # print(f"scale: {int(xs[i] / STEP)*10}%")
-    # top = top_k.top_k(domain_list, n, window_len, THRESHOLD)
+        print(f"scale: {int(xs[i] / STEP)*10}%")
+        top = top_k.top_k(domain_list, n, window_len, k, THRESHOLD)
 
-    # t2 = time.clock_gettime_ns(time.CLOCK_REALTIME)
+        t2 = time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)
 
-    # ys[i] = round((t2 - t1) / 10e9, 3)
+        ys[i] = round((t2 - t1) / 10e9, 3)
+        print(f"  ttc: {ys[i]} seconds")
+        print(top)
     # with open(f"../data/topk/top-{window_len}.txt", 'a') as fp:
     #     for pair in top_k:
     #         fp.write(f"{pair[0]},{pair[1]}\n")
@@ -48,8 +52,8 @@ def main():
 
     # plt.plot(xs, ys, color=mcolors.BASE_COLORS["k"])
     # marker, stemlines, baseline = plt.stem(xs, ys)
-    # plt.setp(marker, "color", mcolors.BASE_COLORS["k"])
-    # plt.setp(stemlines, "color", mcolors.BASE_COLORS["k"])
+    # plt.setp(marker, "color", "grey")
+    # plt.setp(stemlines, "color", "grey")
     # plt.setp(stemlines, "linestyle", "dotted")
     # plt.setp(baseline, "color", "grey")
 
