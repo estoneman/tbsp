9912c4cd93b3b01bf7b3c502d4d5f8db6b19ef80
estoneman <etst9006@colorado.edu>
2023-04-05 13:08:43 -0600

    no html upon build

--- py/setup.py
+++ py/setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-ext_options = {"compiler_directives": {"profile": True, "language_level": 3},
-               "annotate": True}
+ext_options = {"compiler_directives": {"profile": True, "language_level": 3}}
 
 extensions = [Extension("edit_distance", ["edit_distance.pyx"])]
 setup(
