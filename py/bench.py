fb1b757e687b3993e724ed5d81811a2a0a70953e
estoneman <etst9006@colorado.edu>
2023-04-05 23:50:21 -0600

    update interface with newer sliding windows system

--- py/bench.py
+++ py/bench.py
@@ -7,8 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-import top_k
-from util import fetch_lines
+import win_proc
+from win_util import fetch_lines
+from win_stat import StatType
 
 def main():
     n = 1744
@@ -27,38 +28,39 @@ def main():
         # )
         window_len = int(n * (xs[i]/100))
         k = 5
+        FLAGS = StatType.MAX.value | \
+                StatType.MIN.value | \
+                StatType.TOP_K.value
 
         t1 = time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)
 
         print(f"scale: {int(xs[i] / STEP)*10}%")
-        top = top_k.top_k(domain_list, n, window_len, k, THRESHOLD)
+        top = win_proc.process_windows(domains, n, window_len,
+                                       THRESHOLD, FLAGS)
 
         t2 = time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)
 
         ys[i] = round((t2 - t1) / 10e9, 3)
         print(f"  ttc: {ys[i]} seconds")
         print(top)
-    # with open(f"../data/topk/top-{window_len}.txt", 'a') as fp:
-    #     for pair in top_k:
-    #         fp.write(f"{pair[0]},{pair[1]}\n")
 
-    # plt.figure(1)
+    plt.figure(1)
 
-    # plt.title(f"Sliding Window @ {n=} Domains")
-    # plt.xlabel("window size (% domain size)")
-    # plt.ylabel("time (s)")
+    plt.title(f"Sliding Window @ {n=} Domains")
+    plt.xlabel("window size (% domain size)")
+    plt.ylabel("time (s)")
 
-    # plt.xticks(xs)
+    plt.xticks(xs)
 
-    # plt.plot(xs, ys, color=mcolors.BASE_COLORS["k"])
-    # marker, stemlines, baseline = plt.stem(xs, ys)
-    # plt.setp(marker, "color", "grey")
-    # plt.setp(stemlines, "color", "grey")
-    # plt.setp(stemlines, "linestyle", "dotted")
-    # plt.setp(baseline, "color", "grey")
+    plt.plot(xs, ys, color=mcolors.BASE_COLORS["k"])
+    marker, stemlines, baseline = plt.stem(xs, ys)
+    plt.setp(marker, "color", "grey")
+    plt.setp(stemlines, "color", "grey")
+    plt.setp(stemlines, "linestyle", "dotted")
+    plt.setp(baseline, "color", "grey")
 
-    # print("Plot is shown")
-    # plt.show()
+    print("Plot is shown")
+    plt.show()
 
 if __name__ == "__main__":
     main()
