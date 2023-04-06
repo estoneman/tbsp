8c2a784d831f09fd089d6c1f9ebdc37c9b3cf10d
estoneman <etst9006@colorado.edu>
2023-04-05 23:51:11 -0600

    update linting rules

--- py/linter.sh
+++ py/linter.sh
@@ -7,8 +7,10 @@ for file in `find . -maxdepth 1 -type f -name '*.py'` ; do
     pylint --spelling-private-dict $DICT \
            --max-line-length 80 \
            --min-similarity-lines 1 \
+           --max-args 6 \
            --max-locals 20 \
            --check-quote-consistency y \
-           --good-names i,j,k,n,w \
+           --good-names i,j,k,n,w,x,Type,get_Type \
+           --extension-pkg-allow-list edit_distance \
            $file
 done
