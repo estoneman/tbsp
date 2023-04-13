4c4fc33eda6d6ce36faee8c5df4d18bc0ca2a386
estoneman <etst9006@colorado.edu>
2023-04-13 07:59:07 -0600

    add unit tests

--- py/test.py
+++ py/test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import win_util
@@ -15,6 +16,23 @@ class TestTbsp(unittest.TestCase):
         stripped = win_util.remove_tld(domain)
 
         self.assertEqual(stripped, "dns1.google")
+
+    def test_is_iterable(self):
+        x = 10
+        is_it = win_util.is_iterable(x)
+
+        self.assertFalse(is_it)
+
+    def test_fetch_lines(self):
+        fn = "sample.txt"
+        with open(fn, "w") as fp:
+            fp.write("google.com\n")
+            fp.write("chat.openai.com\n")
+
+        lines = list(win_util.fetch_lines(fn))
+        os.unlink(fn)
+
+        self.assertEqual(len(lines), 2)
 
 if __name__ == "__main__":
     unittest.main()
