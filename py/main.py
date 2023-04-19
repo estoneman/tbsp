a98ad9f3ffe472aa67a315a533b199f9cbdde80a
estoneman <etst9006@colorado.edu>
2023-04-18 21:20:38 -0600

    update main module

--- py/main.py
+++ py/main.py
@@ -10,34 +10,20 @@ from win_util import fetch_lines
 from win_stat import StatType
 
 def main():
-    """Main method (for now) for testing sliding windows implementation"""
-    n = 500
-    THRESHOLD = 0.000
-    START = 50
+    """Sample run of processing sliding windows of domains"""
+    n = 1744
+    THRESHOLD = 0.600
+    START = 30
 
-    # domain_list = fetch_lines("../data/domains.in")
-    domain_list = fetch_lines("/usr/local/opt/wordlists/rockyou/rockyou.txt")
+    domain_list = fetch_lines("../data/domains.in")
     window_len = int(n * (START/100))
     FLAGS = StatType.MAX.value | \
             StatType.MIN.value | \
             StatType.TOP_K.value | \
             StatType.MEAN.value | \
             StatType.STD_DEV.value
+
+    win_proc.process_windows(domain_list, n, window_len, THRESHOLD, FLAGS)
     
-    sim_mat = win_proc.process_windows(domain_list, n, window_len,
-                                       THRESHOLD, FLAGS)
-
-    # === PLOT DATA COVERAGE ===
-    plt.figure(1)
-    nonzero_mask = sim_mat != 0
-    rows, cols = np.nonzero(nonzero_mask)
-    values = sim_mat[nonzero_mask]
-
-    plt.scatter(rows, cols, c=values)
-    plt.title("Sliding Window Data Coverage")
-
-    print("PLOT IS SHOWN")
-    plt.show()
-
 if __name__ == "__main__":
     main()
