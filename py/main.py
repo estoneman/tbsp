66b25521b3a7be3fdd7ac11edb6e905d210f0c1e
estoneman <etst9006@colorado.edu>
2023-05-02 09:19:59 -0600

    physically align flags

--- py/main.py
+++ py/main.py
@@ -6,14 +6,16 @@ from win_stat import StatType
 def main():
     """Sample run of processing sliding windows of domains"""
     n = 1744 # total length of input file
+    # n = 100000
     THRESHOLD = 0.600
 
     domain_list = fetch_lines("../data/domains.in")
-    FLAGS = StatType.MAX.value | \
-            StatType.MIN.value | \
-            StatType.TOP_K.value | \
-            StatType.MEAN.value | \
-            StatType.STD_DEV.value
+    # domain_list = fetch_lines("/usr/local/opt/wordlists/rockyou/rockyou.txt")
+    FLAGS = StatType.MAX.value     \
+            | StatType.MIN.value   \
+            | StatType.TOP_K.value \
+            | StatType.MEAN.value  \
+            | StatType.STD_DEV.value
 
     win_proc.process_windows(domain_list, n, THRESHOLD, FLAGS)
 
